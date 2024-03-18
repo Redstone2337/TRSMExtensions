924bcf5d2348d3e374d7526896d314cc0141ea8f
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-18 23:13:26 +0800

    Add files via upload

---
+++ src/main/java/com/redstone233/trsm/extension/TRSMExtensionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.redstone233.trsm.extension;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TRSMExtensionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
